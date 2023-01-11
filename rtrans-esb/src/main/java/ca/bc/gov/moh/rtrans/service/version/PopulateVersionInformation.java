d23d8ec2ecc7f4941f87a9f75c4ae6586d24b43f
prit-cgi <prit.thakkar@cgi.com>
2023-01-11 15:13:03 -0400

    addressed review comments

--- rtrans-esb/src/main/java/ca/bc/gov/moh/rtrans/service/version/PopulateVersionInformation.java
+++ rtrans-esb/src/main/java/ca/bc/gov/moh/rtrans/service/version/PopulateVersionInformation.java
@@ -1,7 +1,5 @@
 package ca.bc.gov.moh.rtrans.service.version;
 
-import java.util.Optional;
-
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.slf4j.Logger;
@@ -11,8 +9,8 @@ import ca.bc.gov.moh.rtrans.RTransMainMethod;
 import net.minidev.json.JSONObject;
 
 /**
- * This class is created to get the manifest information and provide the same in version route as JSON object.
- * Manifest information is set in pom.xml 
+ * This class is created to get the manifest information and provide the same in
+ * version route as JSON object. Manifest information is set in pom.xml
  */
 public class PopulateVersionInformation implements Processor {
 	private static final Logger logger = LoggerFactory.getLogger(PopulateVersionInformation.class);
@@ -20,18 +18,18 @@ public class PopulateVersionInformation implements Processor {
 	private static final String VERSION_INFORMATION = getVersionInformation().toJSONString();
 
 	/**
-	 * Loading the version information using Package class.
-	 * Version information is set in jar's META-INF/Manifest.mf file
+	 * Loading the version information using Package class. Version information is
+	 * set in jar's META-INF/Manifest.mf file
+	 * 
 	 * @return JSONObject
 	 */
 	public static JSONObject getVersionInformation() {
 		Package pck = RTransMainMethod.class.getPackage();
-		final String methodName = getMethodName();
 		// init a JSON object
 		JSONObject v2JsonObj = new JSONObject();
 		v2JsonObj.put(IMPLEMENTATION_VERSION_KEY, pck.getImplementationVersion());
 		String jsonString = v2JsonObj.toJSONString();
-		logger.debug("{} - The JSON Message is: {}", methodName, jsonString);
+		logger.debug("{} - The JSON Message is: {}", jsonString);
 		// return the JSON object
 		return v2JsonObj;
 	}
@@ -39,17 +37,6 @@ public class PopulateVersionInformation implements Processor {
 	@Override
 	public void process(Exchange exchange) throws Exception {
 		exchange.getIn().setBody(VERSION_INFORMATION);
-	}
-
-	/**
-     * This method uses StackWalker API to get the names of the current calling method 
-     * @return
-     */
-	private static String getMethodName() {
-		StackWalker walker = StackWalker.getInstance();
-		Optional<String> methodName = walker.walk(frames -> frames.limit(2).skip(1) // to get name of caller
-				.findFirst().map(StackWalker.StackFrame::getMethodName));
-		return methodName.orElse("Method name Unknnown");
 	}
 
 }
--- rtrans-esb/src/main/java/ca/bc/gov/moh/rtrans/service/version/VersionRoute.java
+++ rtrans-esb/src/main/java/ca/bc/gov/moh/rtrans/service/version/VersionRoute.java
@@ -4,21 +4,20 @@ import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 
 /**
- * VersionRoute class is added to provide the version information of deployed artifacts
- * This information can be retrieved from URL: http://<hostname>:<port>/version
+ * VersionRoute class is added to provide the version information of deployed
+ * artifacts This information can be retrieved from URL:
+ * http://<hostname>:<port>/version
  * 
  */
 public class VersionRoute extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-        from("jetty:http://{{minaListenerURI}}:{{port}}/version").routeId("rtrans-version")
-    	.log("RTrans received a request for version information")
-    	.to("log:HttpLogger?level=DEBUG&showBody=true&multiline=true")
-    	.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-    	.process(new PopulateVersionInformation())
-    	.log("RTrans sent a response with version information")
-    	;
+		from("jetty:http://{{minaListenerURI}}:{{port}}/version").routeId("rtrans-version")
+				.log("RTrans received a request for version information")
+				.to("log:HttpLogger?level=DEBUG&showBody=true&multiline=true")
+				.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+				.process(new PopulateVersionInformation()).log("RTrans sent a response with version information");
 
 	}
 
